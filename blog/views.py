c02465f6331e918af57da79d155e1ae0b82e9eec
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-15 14:01:09 +0100

    Users can now add comments on blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
+from .forms import CommentForm
 
 def get_date(post):
     return post['date']
@@ -38,17 +41,32 @@ class AllPostsView(ListView):
 #         "all_posts": all_posts
 #     })
 
-class PostDetailView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
     
-
-
 # def post_detail(request, slug):
 #     this_post = get_object_or_404(Post, slug=slug)
 #     return render(request, "blog/post-detail.html", {
