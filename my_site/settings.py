5bb6622d65b5ff56c90809b09e86dd2a359f9058
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-19 17:30:45 +0100

    Now storing media files and static files in a AWS S3 Bucket in separate folders

--- my_site/settings.py
+++ my_site/settings.py
@@ -162,4 +162,8 @@ AWS_ACCESS_KEY_ID = getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = getenv("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = getenv("AWS_S3_CUSTOM_DOMAIN")
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_FOLDER = "static"
+MEDIAFILES_FOLDER = "media"
+
+STATICFILES_STORAGE = "custom_storages.StaticFileStorage"
+DEFAULT_FILE_STORAGE = "custom_storages.MediaFileStorage"
