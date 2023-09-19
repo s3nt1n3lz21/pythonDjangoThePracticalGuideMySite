61b18e0d4cb9946fcc2f514caa79d86e8f7ed960
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-19 11:25:24 +0100

    Setup environment variables in settings.py ready for deployment

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +24,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xm@20&3^m-gsr4l9$doczfi_*b&bzk9ah$6%^z@cl01-gwt8@$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+if DEBUG == True:
+    load_dotenv()
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv("SECRET_KEY")
+
+if DEBUG == True:
+    ALLOWED_HOSTS = ['127.0.0.1']
+else:
+    ALLOWED_HOSTS = [
+        getenv("APP_HOST")
+    ]
 
 # Application definition
 
